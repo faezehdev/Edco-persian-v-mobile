a91308b69ed57a302b78f7f28234e5cbd545c9dd
faezehdev <faezeh0078@gmail.com>
2025-01-11 17:01:39 +0330

    complete blog page

---
+++ js/blog-mobile.js
@@ -0,0 +1,41 @@
+// show more project
+let blogs =[...document.querySelectorAll('.blog')]
+document.addEventListener('DOMContentLoaded',()=>{
+    let visibleCount = 4
+    let itemPerpage = 4
+    let loadMoreBtn = document.querySelector('.showMoreBTN')
+    if(blogs.length < 4){
+   loadMoreBtn.style.display='none'
+    }
+ 
+    blogs.map((pro,i)=>{
+        console.log(pro);
+        
+        if(i<4){
+            pro.classList.add('show')
+            
+        }
+       
+
+        })
+      
+         let showMoreItems = ()=>{
+           
+            
+            for(let i = visibleCount; i < visibleCount + itemPerpage; i++){
+                if(blogs[i]){
+                    blogs[i].classList.add('show')
+                }
+            }
+            console.log('visibleCount',visibleCount);
+            visibleCount += itemPerpage
+            
+            if(visibleCount >= blogs.length){
+                loadMoreBtn.style.display='none'
+            }
+         }
+         loadMoreBtn.addEventListener('click',()=>{
+            showMoreItems()
+         })
+})
+
