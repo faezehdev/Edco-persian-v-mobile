fb7282c0861e5cbc31577e0aac6a099d544fc549
faezehdev <faezeh0078@gmail.com>
2025-01-11 16:41:58 +0330

    complete project page

--- js/project-mobile.js
+++ js/project-mobile.js
@@ -4,6 +4,10 @@ let Swiperpro= new Swiper ('.swiper-products', {
     spaceBetween: 100,
    speed:500,
    grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
    navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
