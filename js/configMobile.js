bc8732acb2188923e8d999279f343e7e89117378
faezehdev <faezeh0078@gmail.com>
2025-01-12 14:50:29 +0330

    complete contactus page

--- js/configMobile.js
+++ js/configMobile.js
@@ -3,9 +3,18 @@ let menuContainer = document.querySelector('.MB-menu')
 let toggle = document.querySelectorAll('.openNav .toggle')
 let menuItems = document.querySelectorAll('.MB-menu ul li')
 let Isopen = false
+if(document.querySelector('.ContactUS-mobile')){
+  console.log('ContactUS page');
+  let header = document.querySelector('header')
+  header.classList.add('whiteHeader')
+  }
 menuIcon.addEventListener('click',()=>{
   if(!Isopen){
     menuContainer.classList.toggle('open')
+    if(document.querySelector('.ContactUS-mobile')){
+      let header = document.querySelector('header')
+      header.classList.add('darkHeader')
+      }
     let toggles = document.querySelectorAll('#mobile-header .right .mobile-menu .openNav .toggle')
     document.querySelector('#mobile-header .right .mobile-menu .openNav').style.transform='translateY(-2px)';
     toggle.forEach((t)=>{
@@ -18,6 +27,10 @@ menuIcon.addEventListener('click',()=>{
     Isopen =true
   }
   else{
+    if(document.querySelector('.ContactUS-mobile')){
+      let header = document.querySelector('header')
+      header.classList.remove('darkHeader')
+      }
     menuContainer.classList.toggle('open')
     let toggles = document.querySelectorAll('#mobile-header .right .mobile-menu .openNav .toggle')
     toggles.forEach((t)=>{
@@ -31,7 +44,10 @@ menuIcon.addEventListener('click',()=>{
   }
  
 })
-
+if(document.querySelector('.ContactUS-mobile')){
+let header = document.querySelector('header')
+header.classList.add('whiteHeader')
+}
 let langIcon = document.querySelector('.langIcon')
 let langSVG = document.querySelector('.langIcon svg')
 let langContent = document.querySelector('.langs')
@@ -56,8 +72,16 @@ lenis.on('scroll',(args)=>{
     let header = document.querySelector('header')
     if( args.scroll == 0){
         header.classList.remove('goDown')
+        if(document.querySelector('.ContactUS-mobile')){
+          let header = document.querySelector('header')
+          header.classList.add('whiteHeader')
+          }
     }
     if( args.scroll > 0){
+      if(document.querySelector('.ContactUS-mobile')){
+        let header = document.querySelector('header')
+        header.classList.remove('whiteHeader')
+        }
         header.classList.add('goDown')
     }
     
