c28e6357b9f8b4a4968a6e2f9df96eb67af87060
faezehdev <faezeh0078@gmail.com>
2025-01-12 11:05:05 +0330

    complete product page

--- js/product-mobile.js
+++ js/product-mobile.js
@@ -1,60 +1,6 @@
-let fixSec = document.querySelector('.FixedSec .sideMenu')
-let sections = document.querySelectorAll('.section')
-let menuActive = document.querySelector('.menu__active')
-let btns = document.querySelectorAll('.menu__link');
-lenis.on('scroll',(e)=>{
-    if(e.scroll==0){
-        fixSec.classList.remove('fixsec')
-        menuActive.classList.remove('fixsec')
-    }
-    if(e.scroll>10){
-        console.log(e);
-        
-        fixSec.classList.add('fixsec')
-        menuActive.classList.add('fixsec')
-        let scrTop = e.scroll
-             // change active menu on scroll           
-             sections.forEach(sec =>{
-                let top = scrTop;
-                let offset = sec.offsetTop-200;
-                let height= sec.offsetHeight;
-                let id =sec.getAttribute('id')
-                if(top>= offset && top < offset + height){
-                btns.forEach(btn=>{         
-                btn.classList.remove('active');
-                document.querySelector('.menu__list .menu__item a[href*='+id + ']').classList.add('active')
-                menuActive.style.top =`calc(${(document.querySelector('.menu__list .menu__item a[href*='+id + ']').offsetTop)}px)` 
-                     })
-                 }
-            })
 
-    }
 
-    
-})
 
-btns.forEach((btn,i)=>{         
-    btn.addEventListener('click',(e)=>{
-        e.preventDefault()
-        if(i==0){
-            console.log('first');
-            lenis.scrollTo(0,{offset:0})
-            for(let p=0 ; p< btns.length ; p++){
-                btns[p].classList.remove('active');
-              }
-              btn.classList.add('active')
-        }
-        else{
-            for(let p=0 ; p< btns.length ; p++){
-                btns[p].classList.remove('active');
-              }
-              btn.classList.add('active');
-              let target = e.currentTarget.getAttribute('href')
-              lenis.scrollTo(`${target}`,{offset:-190})
-        }
-  
-   });
-   })
 let IS = false
 // show more content
 let items =[...document.querySelectorAll('#section-2 .Schemass .Schema')]
@@ -107,7 +53,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 // product related slider
 let Swiperpro= new Swiper ('.swiper-products', {
     slidesPerView: "auto",
-    spaceBetween: 100,
+    spaceBetween: 10,
    speed:500,
    grabCursor: true,
    navigation: {
@@ -123,7 +69,7 @@ let Swiperpro= new Swiper ('.swiper-products', {
 // projec related slider
 let Swiperproject= new Swiper ('.swiper-projects', {
     slidesPerView: "auto",
-    spaceBetween: 50,
+    spaceBetween: 10,
    speed:500,
    grabCursor: true,
    navigation: {
